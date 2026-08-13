3f74837fd557d69cc67d493fae71240321eefbe7
msman20128-lgtm <msman20128@gmail.com>
2026-08-13 14:55:10 +0530

    Delete app/frontend/test directory

--- app/frontend/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:alt1/main.dart';
-
-void main() {
-  testWidgets('App renders Healthcare Companion App', (WidgetTester tester) async {
-    await tester.pumpWidget(const HealthcareCompanionApp());
-    expect(find.byType(HealthcareCompanionApp), findsOneWidget);
-  });
-}
